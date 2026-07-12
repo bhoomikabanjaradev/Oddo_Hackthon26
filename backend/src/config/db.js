7056ce08098c76e8171207b156ba46b7a0eebe37
devankitx <ankitmaurya.coder@gmail.com>
2026-07-12 11:51:34 +0530

    feat: seed one admin user for now

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export async function connectDB() {
+export default async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB Connected");
