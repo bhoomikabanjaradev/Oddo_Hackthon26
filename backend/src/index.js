528ca3aa5ee76a43b491985b9b0122ed0f26ba02
bipinsbisht <bipinsbishtofficial@gmail.com>
2026-07-12 12:17:28 +0530

    Add vehicle CRUD: model fix, controller, routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import vehicleRoutes from "./routes/vehicleRoutes.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-dotenv.config();
-app.post("/", (req, res) => {
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.use("/vehicles", vehicleRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
