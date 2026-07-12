3652e681511c7aa29450c47117d5dbddfe9a1d69
bipinsbisht <bipinsbishtofficial@gmail.com>
2026-07-12 12:40:09 +0530

    Revert index.js to main version

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,15 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import vehicleRoutes from "./routes/vehicleRoutes.js";
 
 dotenv.config();
+import routes from "./routes/index.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
-connectDB();
+app.use("/api/v1", routes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.use("/vehicles", vehicleRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
